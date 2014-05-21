d078262edfdfbbb10147c69855f64871425f8a75
unknown <jlaw@C00823.usurf.usu.edu>
2014-05-21 08:31:32 -0600

    Started functionality for filters.

--- site/app/js/common/pagetransitions.js
+++ site/app/js/common/pagetransitions.js
@@ -1,19 +1,19 @@
 var PageTransitions = (function() {
 
 	var $main = $( '#pt-main' ),
-		$pages = $main.children( 'div.pt-page' ),
-		$iterate = $( '#showPageLeft' ),
-		animcursor = 1,
-		pagesCount = $pages.length,
-    current = 0,
-		next = 1,
-    isAnimating = false,
-		animEndEventNames = {
-			'WebkitAnimation' : 'webkitAnimationEnd',
-      'OAnimation' : 'oAnimationEnd',
-			'msAnimation' : 'MSAnimationEnd',
-			'animation' : 'animationend'
-		},
+  $pages = $main.children( 'div.pt-page' ),
+  $iterate = $( '#showPageLeft' ),
+  animcursor = 1,
+  pagesCount = $pages.length,
+  current = 0,
+  next = 1,
+  isAnimating = false,
+  animEndEventNames = {
+   'WebkitAnimation' : 'webkitAnimationEnd',
+   'OAnimation' : 'oAnimationEnd',
+   'msAnimation' : 'MSAnimationEnd',
+   'animation' : 'animationend'
+ },
 		// animation end event name
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
 
@@ -21,25 +21,25 @@ var PageTransitions = (function() {
     'WebkitTransition' : 'webkitTransitionEnd',// Saf 6, Android Browser
     'MozTransition'    : 'transitionend',      // only for FF < 15
     'transition'       : 'transitionend'       // IE10, Opera, Chrome, FF 15+, Saf 7+
-    },
-    
+  },
+
     //transition end event names
     transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ];
 		// support css animations
 		support = Modernizr.cssanimations;
-	
-	function init() {
+
+   function init() {
 
 
-		$pages.each( function() {
-			var $page = $( this );
-      $page.removeClass("pt-page-current");
-      $page.removeClass("pt-page-not-current");
-			$page.data( 'originalClassList', $page.attr( 'class' ) );
-		} );
+    $pages.each( function() {
+     var $page = $( this );
+     $page.removeClass("pt-page-current");
+     $page.removeClass("pt-page-not-current");
+     $page.data( 'originalClassList', $page.attr( 'class' ) );
+   } );
 
     $pages.eq( current ).addClass( 'pt-page-current' );
-		$pages.eq( next ).addClass( 'pt-page-not-current' );
+    $pages.eq( next ).addClass( 'pt-page-not-current' );
 
     $(document).ready(function(){
 
@@ -85,78 +85,169 @@ var PageTransitions = (function() {
     }
 
     var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
-      outClass = '', inClass = '';
+    outClass = '', inClass = '';
     var move = $(window).width();
     console.log("move", move);
     
     switch( animation ) {
 
       case 1:
-        outClass = 'pt-page-moveToLeft';
-        inClass = 'pt-page-moveFromRight';
-        $('#showPageLeft').css("left", move+"px");
-        $('#showPageLeft').css("right", "auto");
-        $('#showPageLeft').toggleClass('current').animate({
-          left: "-="+(move - 300)
+      outClass = 'pt-page-moveToLeft';
+      inClass = 'pt-page-moveFromRight';
+      $('#showPageLeft').css("left", move+"px");
+      $('#showPageLeft').css("right", "auto");
+      $('#showPageLeft').toggleClass('current').animate({
+        left: "-="+(move - 300)
           // left: "-="+move,
         }, 200, function() {
           $('#showPageLeft').html("<i class='fa fa-caret-right'></i>")
         // Animation complete.
-        });
-        break;
+      });
+      break;
       case 2:
-        outClass = 'pt-page-moveToRight';
-        inClass = 'pt-page-moveFromLeft';
+      outClass = 'pt-page-moveToRight';
+      inClass = 'pt-page-moveFromLeft';
 
-        $('#showPageLeft').css("left", "auto");
-        $('#showPageLeft').css("right", (move - 300)+"px");
-        $('#showPageLeft').toggleClass('current').animate({
-          right: "-="+(move - 300)
-        }, 200, function() {
-          $('#showPageLeft').html("<i class='fa fa-caret-left'></i>")
-        });
-        break;
+      $('#showPageLeft').css("left", "auto");
+      $('#showPageLeft').css("right", (move - 300)+"px");
+      $('#showPageLeft').toggleClass('current').animate({
+        right: "-="+(move - 300)
+      }, 200, function() {
+        $('#showPageLeft').html("<i class='fa fa-caret-left'></i>")
+      });
+      break;
     }
 
     $currPage.addClass( outClass ).on( animEndEventName, function() {
       $currPage.off( animEndEventName );
-        onEndAnimation( $currPage, $nextPage );
+      onEndAnimation( $currPage, $nextPage );
     }).on( transEndEventName, function() {
       $currPage.off( transEndEventName );
-        onEndAnimation( $currPage, $nextPage );
+      onEndAnimation( $currPage, $nextPage );
     } );
 
     $nextPage.addClass( inClass ).on( animEndEventName, function() {
       $nextPage.off( animEndEventName );
-        onEndAnimation( $currPage, $nextPage );
+      onEndAnimation( $currPage, $nextPage );
     }).on( transEndEventName, function() {
       $nextPage.off( transEndEventName );
-				onEndAnimation( $currPage, $nextPage );
-		} );
+      onEndAnimation( $currPage, $nextPage );
+    } );
 
-		if( !support ) {
-			onEndAnimation( $currPage, $nextPage );
-		}
+    if( !support ) {
+     onEndAnimation( $currPage, $nextPage );
+   }
 
-	}
+ }
 
-	function onEndAnimation( $outpage, $inpage ) {
-    console.log("We reset");
-    
-		resetPage( $outpage, $inpage );
-    isAnimating = false;
-	}
+ function onEndAnimation( $outpage, $inpage ) {
+  console.log("We reset");
 
-	function resetPage( $outpage, $inpage ) {
-    console.log("Out class", $outpage.data('originalClassList'));
-    console.log("In class", $inpage.data('originalClassList'));
-    
-		$outpage.attr( 'class', $outpage.data( 'originalClassList') + ' pt-page-not-current');
-		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
-	}
+  resetPage( $outpage, $inpage );
+  isAnimating = false;
+}
 
-	init();
+function resetPage( $outpage, $inpage ) {
+  console.log("Out class", $outpage.data('originalClassList'));
+  console.log("In class", $inpage.data('originalClassList'));
 
-	return { init : init };
+  $outpage.attr( 'class', $outpage.data( 'originalClassList') + ' pt-page-not-current');
+  $inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+}
+
+init();
+
+return { init : init };
 
 })();
+
+var elementPosition = $('.filters').offset();
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > elementPosition.top){
+    $('.filters').css({'position':'fixed','top':'0'});
+    $('.filtersOverride').css({'height': '100%'})
+  } else {
+    var height = $(window).height() - (0 - ($(window).scrollTop() - elementPosition.top));
+    console.log("height", height);
+
+    $('.filters').css({'position':'absolute', 'top':'52px'});
+    $('.filtersOverride').css({'height': height})
+  }
+});
+$(document).ready(function(){
+  var elementPosition = $('.filters').offset();
+  if($(window).scrollTop() > elementPosition.top){
+    $('.filters').css({'position':'fixed','top':'0'});
+    $('.filtersOverride').css({'height': '100%'})
+  } else {
+    var height = $(window).height() - (0 - ($(window).scrollTop() - elementPosition.top));
+    console.log("height", height);
+    $('.filters').css({'position':'absolute', 'top':'52px'});
+    $('.filtersOverride').css({'height': height})
+  }    
+  var toggled = false;
+  $("#showFilters").on("click", function(){
+    if (!toggled){
+      toggled = true;
+      showFilters();
+    }
+    else {
+      toggled = false;
+      hideFilters();
+    }
+  });
+  function showFilters(){
+    $('.filters').toggleClass("filtersOverride");
+    $( '.filterButton' ).animate({
+      right: "+=100px",
+      width: "300px"
+    }, 300, function() {
+    });
+    $( '.filtersOverride' ).animate({
+      left: "+=300px"
+    }, 300, function() {
+      $('#showFilters').html("Filters <i class='fa fa-caret-left'></i>");
+    });
+    $( '#pt-main' ).animate({
+      'margin-left': "+=300px"
+    }, 200, function() {
+      $( '#page1' ).animate({
+        'padding-top': "-=40px"
+      }, 100, function() {
+      });
+    });
+    $( '#showPageLeft' ).animate({
+      'margin-left': "+=300px"
+    }, 200, function() {
+    });
+  }
+  function hideFilters(){
+    $( '.filterButton' ).animate({
+      right: "-=100px",
+      width: "100px"
+    }, 300, function() {
+    });
+    $( '.filtersOverride' ).animate({
+      left: "-=300px"
+    }, 300, function() {
+      $('#showFilters').html("Filters <i class='fa fa-caret-right'></i>");
+      $('.filters').toggleClass("filtersOverride");
+    });
+    $( '#page1' ).animate({
+      'padding-top': "+=40px"
+    }, 100, function() {
+      $( '#pt-main' ).animate({
+        'margin-left': "-=300px"
+      }, 200, function() {
+      });
+    });
+    setTimeout(function() {
+      $( '#showPageLeft' ).animate({
+        'margin-left': "-=300px"
+      }, 200, function() {
+      });
+    }, 100);
+  }
+
+});
