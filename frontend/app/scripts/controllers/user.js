3fd7efcb8afe999eb16bf09acf4c897d6992a2b5
unknown <jlaw@C00823.usurf.usu.edu>
2014-05-30 16:44:33 -0600

    Initial commit for yoeman setup on the master branch frontend

---
+++ frontend/app/scripts/controllers/user.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('frontendApp')
+  .controller('UserCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
