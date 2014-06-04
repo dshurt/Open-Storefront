2a12c283ba027db58c97a664b8873b864455e934
unknown <jlaw@C00823.usurf.usu.edu>
2014-06-04 16:48:04 -0600

    Added comments to the filter for future use.

--- client/openstorefront/app/scripts/filters/componentfilter.js
+++ client/openstorefront/app/scripts/filters/componentfilter.js
@@ -1,5 +1,70 @@
 'use strict';
 
+
+/*******************************************************************************
+* This filter requires that you pass in the filters ;) it is an array of objects
+* in this format:
+*[
+*  {
+*    "name": "Types",
+*    "key": "type",   // The key for the attribute in the object you're looking for
+*    "collection": [
+*      {
+*        "code": "APPS",  // the value of the attribute in the object you wish to find
+*        "type": "Application", // periferal information
+*        "desc": "Applications are ...",
+*        "src": "images/icon/pastel/locationOfFilterImage.png",
+*        "checked": true // whether or not this portion of the filter defaults to checked (implemented) or not
+*      },
+*      {
+*        "code": "WIDGET",
+*        "type": "Widget",
+*        "desc": "Filter description",
+*        "src": "images/icon/pastel/locationOfFilterImage.png",
+*        "checked": false
+*      }
+*      ...
+*    ]
+*  },
+*  {
+*    "name": "Categories",
+*    "key": "categories", 
+*    "collection": [
+*      {
+*        "code": "MDATA",
+*        "type": "Data Management",
+*        "desc": "Data management is......",
+*        "src": "images/icon/pastel/locationOfFilterImage.png",
+*        "checked": false
+*      },
+*      ...
+*    ]
+*  }
+*]
+*******************************************************************************/
+/*******************************************************************************
+* The object you're looking for must be in a format similar to this:
+*{
+*  "id": 34,
+*  "guid": "6482B5F8-CB71-E00D-8FA4-16529549EC22",
+*  "type": [
+*  {
+*    "code": "APPS"
+*  },
+*  {
+*    "code": "TOOLS"
+*  }
+*  ]
+*  "categories": [
+*  {
+*    "code": "MDATA"
+*  },
+*  {
+*    "code": "COLLECTION"
+*  }
+*  ]
+*}
+*******************************************************************************/
 app.filter('componentFilter', function()  {
   var allChecked = function(collection) {
     var none = _.every(collection, function(item) {
