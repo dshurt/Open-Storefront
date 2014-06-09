796cd68a65a6817bb77f2a9f7e7b98c7346386b9
unknown <jlaw@C00823.usurf.usu.edu>
2014-06-09 15:10:42 -0600

    Added navigation option to modal.

--- client/openstorefront/app/scripts/directives/modal.js
+++ client/openstorefront/app/scripts/directives/modal.js
@@ -9,10 +9,25 @@ angular.module('openstorefrontApp')
     name: 'controllerName',
     template: '<div class="modal fade" id="{{id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> <div ng-include="header"></div> </div> <div class="modal-body"> <div ng-include="body"></div> </div> <div class="modal-footer"> <div ng-include="footer"></div> </div> </div> </div </div>',
     link: function postLink(scope, element, attrs) {
-      scope.header = attrs.header;
-      scope.footer = attrs.footer;
-      scope.body = attrs.body;
+
+      scope.header = 'views/modalDefaults/header.html';
+
+      scope.footer = 'views/modalDefaults/footer.html';
+
+      scope.body = 'views/modalDefaults/body.html';
+      
       scope.id = attrs.modalid;
+
+      if (attrs.header !== null && attrs.header !== undefined) {
+        scope.header = attrs.header;
+      }
+      if (attrs.footer !== null && attrs.footer !== undefined) {
+        scope.footer = attrs.footer;
+      }
+      if (attrs.body !== null && attrs.body !== undefined) {
+        scope.body = attrs.body;
+      }
+
     }
   };
 });
