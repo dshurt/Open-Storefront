450c31d4d5244fd3ff39b2c8579bd50f15e3b763
unknown <jlaw@C00823.usurf.usu.edu>
2014-06-24 17:57:10 -0600

    Started work on the 'more' button.

--- client/openstorefront/app/scripts/directives/componentlist.js
+++ client/openstorefront/app/scripts/directives/componentlist.js
@@ -1,8 +1,9 @@
 'use strict';
 
 app.directive('componentList', ['business', function (Business) {
+  var uniqueId = 1;
   return {
-    template: '<div ng-class="listOfClasses"><div ng-show="isTitle" class="componentListTitle">{{title}}</div><table class="table table-striped" id="resultsTable"><tr ng-repeat="item in data"  style="margin-bottom: 20px;"><td style="white-space: inherit;"><div class="container-fluid"><div class="row" style="margin-left: 0px; margin-right: 0px"><div class="results-content"><div class="results-content-title"><div ng-click="clickCallback(item.id)" id="{{item.id}}"><div class="results-content-title-content" >{{item.name}}</div></div><raty read-only ng-class="{lastPopover:$last}" class="pull-left average-raty" id="star{{$index}}" style="min-width:110px; height: 100%;" score="{{item.stats.averageRating}}" number="5" path="images/raty/" data-container="body" data-toggle="popover" data-content="Average rating: {{item.stats.averageRating}} of {{item.stats.numberRatings}}<br>Comments: {{item.stats.comments}}<br>Views: {{item.stats.views}}"></raty></div><div class="results-content-description">{{item.shortdescription}}</div></div></div></div></td></tr</table></div>',
+    template: '<div class="moreContent"><div class="moreContentArticle"><input ng-show="hasMoreThan3" type="checkbox" role="button" class="read_more"><label ng-show="hasMoreThan3" data-toggle="tooltip" data-placement="top" title="Click here to show more!" ng-click="setShown()" class="read_more circleBase"><span><i class="fa fa-arrow-down"></i></span><span><i class="fa fa-arrow-up"></i></span></label><br /><div ng-class="listOfClasses"><div ng-show="isTitle" class="componentListTitle">{{title}}</div><table class="table table-striped" id="resultsTable"><tr ng-repeat="item in data" class="moreContentSection" style="margin-bottom: 20px;"><td style="white-space: inherit;"><div class="container-fluid"><div class="row" style="margin-left: 0px; margin-right: 0px"><div class="results-content"><div class="results-content-title"><div ng-click="clickCallback(item.id)" id="{{item.id}}"><div class="results-content-title-content" >{{item.name}}</div></div><raty read-only ng-class="{lastPopover:$last}" class="pull-left average-raty" id="star{{$index style="min-width:110px; height: 100%;" score="{{item.stats.averageRating}}" number="5" pathimages/raty/" data-container="body" data-toggle="popover" data-content="Average rating: {{itemstats.averageRating}} of {{item.stats.numberRatings}}<br>Comments: {{item.stats.comments}}<br>Views: {{item.stats.views}}"></raty></div><div class="results-content-description">{{item.shortdescription}}</div></div></div></div></td></tr></table></div></div></div>',
     restrict: 'AE',
     scope: {
       data: '=',
@@ -10,7 +11,23 @@ app.directive('componentList', ['business', function (Business) {
       filters: '='
     },
     link: function postLink(scope, element, attrs) {
+      var item = 'componentList' + uniqueId++;
+      element.find('.moreContent').attr('id', item);
+
+      var item = 'read_more' + uniqueId++;
+      element.find('input.read_more').attr('id', item);
+      element.find('label.read_more').attr('for', item);
+      
+      scope.hasMoreThan3 = false;
+
+      scope.$watch('data', function() {
+        if(scope.data.length > 3) {
+          scope.hasMoreThan3 = true;
+        }        
+      })
+
       scope._scopename = 'componentList';
+      scope.isShownClass = null;
       scope.isTitle = false;
       scope.listOfClasses = attrs.classList;
       if (attrs.type !== null && attrs.type !== undefined && attrs.type !== '') {
@@ -23,6 +40,17 @@ app.directive('componentList', ['business', function (Business) {
         scope.isTitle = true;
         scope.title = attrs.title;
       }
+
+      scope.setShown = function() {
+        var id = element.find('.moreContent').attr('id');
+        $("#" + id).toggleClass("active");
+      };
+
+      scope.init = function() {
+        $('[data-toggle="tooltip"').tooltip();
+      }
+      scope.init();
+
     }
   };
 }]);
